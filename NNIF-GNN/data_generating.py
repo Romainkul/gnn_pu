161c010639c585f0ff61104cfa6fb453ec583f8f
rom1ber <romain.berard007@gmail.com>
2025-04-14 12:19:37 +0200

    change val/train

--- NNIF-GNN/data_generating.py
+++ NNIF-GNN/data_generating.py
@@ -465,7 +465,8 @@ def make_pu_dataset(
     fixed_seed: bool = True,
     sample_seed: int = 5,
     train_pct: float = 0.5,
-    show_count: bool = False
+    show_count: bool = False,
+    val: bool = True,
 ) -> Data:
     """
     Convert a multi-class dataset into a PU setting. We produce:
@@ -597,8 +598,11 @@ def make_pu_dataset(
         pos_list = pos_idx.tolist()
         random.shuffle(pos_list)
         chosen_pos = torch.tensor(pos_list[:pos_train_val], dtype=torch.long)
-        data.train_mask[chosen_pos[:train_pos_num]] = True
-        data.val_mask[chosen_pos] = True
+        if val:
+            data.train_mask[chosen_pos[:train_pos_num]] = True
+            data.val_mask[chosen_pos] = True
+        else:
+            data.train_mask[chosen_pos] = True
 
     elif mechanism.upper() == "SAR":
         if data.x is None:
@@ -614,8 +618,11 @@ def make_pu_dataset(
             probs = dist_mean / dist_mean.sum()
             chosen_ids = torch.multinomial(probs, num_samples=pos_train_val, replacement=False)
             chosen_pos = pos_idx[chosen_ids]
-            data.train_mask[chosen_pos[:pos_train_val]] = True
-            data.val_mask[chosen_pos] = True
+            if val:
+                data.train_mask[chosen_pos[:train_pos_num]] = True
+                data.val_mask[chosen_pos] = True
+            else:
+                data.train_mask[chosen_pos] = True
 
     elif mechanism.upper() == "SAR2" and hasattr(data, 'time'):
         if not hasattr(data, 'time'):
@@ -623,8 +630,11 @@ def make_pu_dataset(
         pos_times = data.time[pos_idx]
         _, sorted_ids = torch.sort(pos_times, descending=False)
         chosen_pos = pos_idx[sorted_ids[:pos_train_val]]
-        data.train_mask[chosen_pos[:train_pos_num]] = True
-        data.val_mask[chosen_pos] = True
+        if val:
+            data.train_mask[chosen_pos[:train_pos_num]] = True
+            data.val_mask[chosen_pos] = True
+        else:
+            data.train_mask[chosen_pos] = True
 
     else:
         raise ValueError(f"Invalid mechanism '{mechanism}'. Use 'SCAR', 'SAR' or 'SAR2'.")    
